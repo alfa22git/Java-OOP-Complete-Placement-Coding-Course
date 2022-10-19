156646447a5c6a3d96c0591fbde847bbe3608fce
ALFA <alfa.sunny.22@gmail.com>
2022-10-19 21:01:58 +0600

    Practices Switch - Break Conditions.

---
+++ src/practices__switch_break_continue.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class practices__switch_break_continue {
+	public static void main(String[] args) {
+		System.out.print("Enter The Input Number : ");
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+
+	}
+}
